e85741b1008484cdc950b8c462fdff4aa7c006d4
Cassano <ninocb@gmail.com>
2018-03-29 15:06:24 -0300

    Mudança nome

--- padraoCommand/src/br/padrão/command/Cliente.java
+++ padraoCommand/src/br/padrão/command/Cliente.java
@@ -2,7 +2,7 @@ package br.padrão.command;
 
 public class Cliente {
 	public static void main(String[] args) {
-		Loja lojasAfricanas = new Loja("Afriacanas");
+		Loja lojasAfricanas = new Loja("Africanas");
 		lojasAfricanas.executarCompra(999.00, new PagamentoCartaoCredito());
 		lojasAfricanas.executarCompra(49.00, new PagamentoBoleto());
 		lojasAfricanas.executarCompra(99.00, new PagamentoCartaoDebito());
